16b0faa1aedb156dd327e2c2289b5428fd970e18
M4T1AS07 <matias.andresps@gmail.com>
2025-09-29 10:38:31 -0300

    Delete portaladopcion directory

--- portaladopcion/portaladopcion/portal/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-class PortalConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'portal'
-
-    def ready(self):
-        from .models import Mascota   # ✅ corregido
